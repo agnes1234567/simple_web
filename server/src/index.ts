a9fa6c7fd31af1e504d75cb882056983c59d254c
Agnes <agnfr981@student.liu.se>
2022-12-24 02:51:28 +0100

    clean

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import routes from './routes/routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
